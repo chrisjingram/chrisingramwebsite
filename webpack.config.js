b272871a9b0325e647190fa7c83a39ce94261888
Chris Ingram <chris@chrisingram.me>
2020-04-03 11:34:01 +0100

    Built scss in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
 // https://dev.to/pixelgoo/how-to-configure-webpack-from-scratch-for-a-basic-website-46a5
 // Webpack uses this to work with directories
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 // This is main configuration object.
 // Here you write different options and tell Webpack what to do
 module.exports = {
 
   // Path to your entry point. From this file Webpack will begin his work
-  entry: './js/index.js',
+  entry: './src/js/index.js',
 
   // Path and filename of your result bundle.
   // Webpack will bundle all JavaScript into this file
@@ -32,7 +33,39 @@ module.exports = {
             presets: ['@babel/preset-env']
           }
         }
+      },
+      {
+        // Apply rule for .sass, .scss or .css files
+        test: /\.(sa|sc|c)ss$/,
+        // Set loaders to transform files.
+        // Loaders are applying from right to left(!)
+        // The first loader will be applied after others
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          {
+            // This loader resolves url() and @imports inside CSS
+            loader: "css-loader",
+          },
+          {
+            // Then we apply postCSS fixes like autoprefixer and minifying
+            loader: "postcss-loader"
+          },
+          {
+            // First we transform SASS to standard CSS
+            loader: "sass-loader",
+            options: {
+              implementation: require("sass")
+            }
+          }
+        ]
       }
     ]
-  }
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "bundle.css"
+    })
+  ]
 };
