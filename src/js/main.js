fb62ebe6e9a1ba6b7839013d4dd5f8c763123dee
Chris Ingram <chris@chrisingram.me>
2021-01-28 12:25:50 +0000

    Anchor links

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,14 @@
+import smoothscroll from 'smoothscroll-polyfill';
 import "../scss/index.scss";
+
+smoothscroll.polyfill();
+
+document.querySelectorAll("[href^='#']").forEach(el => {
+  console.log(el);
+  el.addEventListener('click', e => {
+    e.preventDefault();
+    document.getElementById(el.getAttribute('href').slice(1)).scrollIntoView({behavior: 'smooth'});
+  });
+});
 
 import './mobile-menu';
--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -3,26 +3,37 @@ const mobileMenu = document.getElementById('mobile-menu');
 
 const header = document.querySelector('header');
 
+const open = () => {
+  mobileMenu.style.opacity = "1.0";
+  mobileMenu.style.visibility = "visible";
+  mobileMenu.style.top = `${header.getBoundingClientRect().bottom}px`;
+  mobileMenu.style.height = `${window.innerHeight - header.getBoundingClientRect().height}px`;
+  document.body.scroll = "no";
+  document.body.style.overflow = "hidden";
+  hamburger.classList.add('is-active');
+}
+
+const close = () => {
+  mobileMenu.style.opacity = "0";
+  document.body.scroll = "yes";
+  document.body.style.overflow = "auto";
+  hamburger.classList.remove('is-active');
+  setTimeout(() => {
+    mobileMenu.style.visibility = "hidden";
+  }, 150);
+}
+
+// Hamburger click
 hamburger.addEventListener('click', e => {
-
   e.preventDefault();
-
   if(hamburger.classList.contains('is-active')){
-    mobileMenu.style.opacity = "0";
-    document.body.scroll = "yes";
-    document.body.style.overflow = "auto";
-    hamburger.classList.remove('is-active');
-    setTimeout(() => {
-      mobileMenu.style.visibility = "hidden";
-    }, 150);
+    close();
   }else{
-    mobileMenu.style.opacity = "1.0";
-    mobileMenu.style.visibility = "visible";
-    mobileMenu.style.top = `${header.getBoundingClientRect().bottom}px`;
-    mobileMenu.style.height = `${window.innerHeight - header.getBoundingClientRect().height}px`;
-    document.body.scroll = "no";
-    document.body.style.overflow = "hidden";
-    hamburger.classList.add('is-active');
+    open();
   }
+});
 
+// Close menu on clicking anchor links
+mobileMenu.querySelectorAll("[href^='#'").forEach(link => {
+  link.addEventListener('click', () => close());
 });
