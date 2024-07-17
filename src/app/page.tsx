96ba95cc6cd9d4b9ebe561bb029bd4695eb478c4
Chris Ingram <chris@chrisingram.me>
2024-07-17 13:00:07 +0100

    Experience info

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,9 @@ import meCircle from "../../public/images/me-circle.png";
 const P: React.FC<{ children?: ReactNode }> = ({children}) => <p className="text-base md:text-lg mb-4">{children}</p>
 const H2: React.FC<{ children?: ReactNode }> = ({children}) => <h2 className="text-base md:text-lg mb-4 text-bright-purple font-bold">{children}</h2>
 
-const ExpItem: React.FC<{ companyName: string, tag: string, description: string }> = ({ companyName, tag, description }) => (
+const ExpItem: React.FC<{ companyName: string, tag: string, description: string, url: string }> = ({ companyName, tag, description, url }) => (
   <li className="text-base font-bold md:text-lg pl-[1ch]">
-    <div className="font-bold">{companyName}&nbsp;<span className="text-bright-green">[{tag}]</span></div>
+    <div className="font-bold"><a href={url} target="_blank">{companyName}</a>&nbsp;<span className="text-bright-green">[{tag}]</span></div>
     <p className="font-normal">{description}</p>
   </li>
 )
@@ -36,8 +36,15 @@ export default function Home() {
             <H2>Who do you work with?</H2>
             <P>My freelance clients are usually angel-funded startups or bootstrapped indie businesses.</P>
             <H2>Experience</H2>
-            <ul className="list-style-terminal pl-[1ch]">
-              <ExpItem companyName="Basejam" tag="co-founded" description="The go-to platform for company retreat venues" />
+            <ul className="list-style-terminal pl-[1ch] space-y-2">
+              <ExpItem companyName="Basejam" tag="co-founded" description="The go-to platform for company retreat venues." url="https://basejam.com" />
+              <ExpItem companyName="Saint Storage" tag="freelance/contract" description="Summer storage platform for students." url="https://saintstorage.com" />
+              <ExpItem companyName="Greenpixie" tag="founding team" description="Carbon measurement for the cloud." url="https://greenpixie.com" />
+              <ExpItem companyName="HeyMirza" tag="head of engineering" description="Childcare access platform for workplaces" url="https://heymirza.com" />
+              <ExpItem companyName="MadeBySphere" tag="co-founded" description="Ecommerce design and build agency" url="https://madebysphere.com" />
+              <ExpItem companyName="Boxmove" tag="freelance/contract" description="Ecommerce platform for charity shops" url="https://boxmove.com" />
+              <ExpItem companyName="inCase" tag="senior engineer" description="White-label client communication app for law firms" url="https://in-case.co.uk" />
+              <ExpItem companyName="University of Sheffield / Public Health England" tag="software engineer" description="Active10 walking app for government health initiative" url="https://apps.apple.com/gb/app/nhs-active-10-walking-tracker/id1204295312" />
             </ul>
           </article>
           <aside className="md:flex-1">
