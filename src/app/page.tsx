a62e4329c945ab521336bf74d818f77b177bf19b
Chris Ingram <chris@chrisingram.me>
2024-07-19 15:58:43 +0100

    Removed full stops on availability

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -107,11 +107,11 @@ export default function Home() {
               <Image src={Habitude} alt="Habitude logo" className="w-[119px] h-full" />
             </div> */}
             <H2>Availability</H2>
-            <P className="mb-4">🟢 Short-term freelance/consultancy.</P>
+            <P className="mb-4">🟢 Short-term freelance/consultancy</P>
             <P className="mb-4">🟢 <strong>Free 1-hour consultancy sessions</strong> on technical architecture, AWS, or marketing automation. <A href="#">Book a call</A></P>
             {/* <P className="mb-8"><button className="bg-bright-purple hover:bg-bright-purple-light transition-colors duration-200 text-white rounded-md px-4 py-0.5">Book here &rarr;</button></P> */}
             {/* <P>🟢 I also currently have <strong>availability for longer-term software engineering projects.</strong></P> */}
-            <P className="mb-8">🟢 Longer-term software engineering projects.</P>
+            <P className="mb-8">🟢 Longer-term software engineering projects</P>
             <H2>Get in touch ⬇️</H2>
             <ul className="space-y-1">
               <GetInTouchItem icon={faLinkedin} name="chris-james-ingram" url="https://www.linkedin.com/in/chris-james-ingram/" />
